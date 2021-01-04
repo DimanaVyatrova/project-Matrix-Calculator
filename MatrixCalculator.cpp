484950d25c033ef3132e2696c895b4d9b7b4d6fe
DimanaVyatrova <76838642+DimanaVyatrova@users.noreply.github.com>
2021-01-04 11:08:29 +0200

    Improve the quality of the code

--- MatrixCalculator.cpp
+++ MatrixCalculator.cpp
@@ -4,40 +4,40 @@ using namespace std;
 
 const int MAX_SIZE = 100;
 
-void inputMatrix(double matrix[][MAX_SIZE], int matrixHeight, int matrixWight){
-    for(int i = 0; i < matrixHeight; i++){
-        for(int j = 0; j < matrixWight; j++){
+void inputMatrix(double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
+    for(int i = 0; i < matrixHeight; i++) {
+        for(int j = 0; j < matrixWight; j++) {
             cin >> matrix[i][j];
         }
     }
 }
 
-void outputMatrix(double matrix[][MAX_SIZE], int matrixHeight, int matrixWight){
-    for(int i = 0; i < matrixHeight; i++){
-        for(int j = 0; j < matrixWight; j++){
+void outputMatrix(double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
+    for(int i = 0; i < matrixHeight; i++) {
+        for(int j = 0; j < matrixWight; j++) {
             cout << setw(4) << matrix[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-void multiplMatrixAndNum (double matrix[][MAX_SIZE], int matrixHeight, int matrixWight){
+void multiplMatrixAndNum (double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
     double num = 0;
     cout << "Input number: ";
     cin >> num;
-    for(int i = 0; i < matrixHeight; i++){
-        for(int j = 0; j < matrixWight; j++){
+    for(int i = 0; i < matrixHeight; i++) {
+        for(int j = 0; j < matrixWight; j++) {
             matrix[i][j] = matrix[i][j]*num;
         }
     }
 }
 
-void divideMatrixByNum (double matrix[][MAX_SIZE], int matrixHeight, int matrixWight){
+void divideMatrixByNum (double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
     double num = 0;
     cout << "Input number: ";
     cin >> num;
-    for(int i = 0; i < matrixHeight; i++){
-        for(int j = 0; j < matrixWight; j++){
+    for(int i = 0; i < matrixHeight; i++) {
+        for(int j = 0; j < matrixWight; j++) {
             matrix[i][j] = matrix[i][j]/num;
         }
     }
@@ -53,8 +53,7 @@ int main (){
 
     double matrix[MAX_SIZE][MAX_SIZE];
     inputMatrix(matrix, m, n);
-    divideMatrixByNum(matrix, m, n);
-    outputMatrix(matrix, m, n);
+    outputMatrix(matrix, n, m);
 
 return 0;
 }
