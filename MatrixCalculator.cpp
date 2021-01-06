0fe885ac375e17931df30ddf36ae19732f9deaa3
DimanaVyatrova <76838642+DimanaVyatrova@users.noreply.github.com>
2021-01-06 19:26:14 +0200

    Added functions myPow(), findDet(), find_det_3_by_3,
    Added the functions necessary for calculatind a 4x4 matrix.

--- MatrixCalculator.cpp
+++ MatrixCalculator.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 const int MAX_SIZE = 100;
 
+double myPow (double a, double b){
+    double res = 1;
+    if(b >= 0){
+       for(int i = 0; i < b; i++){
+         res = res*a;
+      }
+    }
+    else {
+        b = -b;
+        for(int i = 0; i < b; i++){
+            res = res*(1/a);
+        }
+    }
+
+    return res;
+}
+
 void inputMatrix(double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
     for(int i = 0; i < matrixHeight; i++) {
         for(int j = 0; j < matrixWight; j++) {
@@ -19,6 +36,7 @@ void outputMatrix(double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) 
         }
         cout << endl;
     }
+    cout << endl;
 }
 
 void multiplMatrixAndNum (double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
@@ -90,6 +108,37 @@ void multiplMatrixByMatrix (double matrix[][MAX_SIZE], int matrixHeight, int mat
     outputMatrix(resultMatrix, matrixHeight, matrix2Wight);
 }
 
+double find_det_3_by_3(double mat[][MAX_SIZE]) {
+    double det = 0;
+
+    det = mat[0][0] * mat[1][1] * mat[2][2] + mat[0][1] * mat[1][2] * mat[2][0] + mat[0][2] * mat[1][0] * mat[2][1]
+        - mat[0][2] * mat[1][1] * mat[2][0] - mat[0][0] * mat[1][2] * mat[2][1] - mat[0][1] * mat[1][0] * mat[2][2];
+
+    return det;
+}
+
+double findDet (double matrix[][MAX_SIZE], int matrixHeight, int matrixWight) {
+    double cofMat[MAX_SIZE][MAX_SIZE];
+    double det = 0;
+
+    for(int c = 0; c < matrixWight; c++) {
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(j >= c){
+                    cofMat[i][j] = matrix[i + 1][j + 1];
+                }
+                else {
+                    cofMat[i][j] = matrix[i + 1][j];
+                }
+            }
+        }
+        //outputMatrix(cofMat, 3, 3);
+        //cout << "det = " << find_det_3_by_3(cofMat) << endl;
+        det = det + matrix[0][c]*myPow(-1, c + 2) * find_det_3_by_3(cofMat);
+    }
+    return det;
+}
+
 int main (){
     int n = 0, m = 0;
     cout << "Input matrix wight: ";
@@ -100,9 +149,14 @@ int main (){
 
     double matrix[MAX_SIZE][MAX_SIZE];
     inputMatrix(matrix, m, n);
-    multiplMatrixByMatrix(matrix, m, n);
+    //findDet(matrix, m, n);
     //outputMatrix(matrix, m, n);
+    cout << findDet(matrix, m, n);
 
 
 return 0;
 }
+/*5 1 -4 0
+-1 2 0 3
+1 1 -7 -2
+5 -3 3 0*/
